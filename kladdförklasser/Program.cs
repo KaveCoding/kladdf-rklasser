e54f1d060878ad92242a60bebfa89abdd6780d1f
KaveCoding <113701512+KaveCoding@users.noreply.github.com>
2022-10-17 15:06:44 +0200

    Add project files.

---
+++ kladdförklasser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace kladdförklasser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
